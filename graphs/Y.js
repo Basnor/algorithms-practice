4464e2ebddb6324dcf2101077f38747e7c218680
Basnor <basnor@mail.ru>
2023-12-23 03:22:48 +0700

    solve Y task with Prim's algorithm

--- graphs/Y.js
+++ graphs/Y.js
@@ -1,86 +1,115 @@
-class Vertex {
-    constructor(start, end, weight) {
-        this.start = start;
-        this.end = end;
-        this.weight = weight;
-    }
-}
-
-class Graph {
-    constructor(vertices, n) {
-        this.vertices = Array.from({ length: n }, (_, i) => i + 1);
-        this.added = new Set(); // Множество вершин, уже добавленных в остов
-        this.notAdded = new Set(this.vertices); // Множество вершин, ещё не добавленных в остов
-        this.edges = new Set(); // Массив рёбер, исходящих из остовного дерева
-        this.adjacencyList = vertices.reduce((list, vertex) => {
-            list.set(vertex.start, list.get(vertex.start).add(vertex));
-            list.set(vertex.end, list.get(vertex.end).add(vertex));
-
-            return list;
-        }, new Map(Array.from({ length: n }, (_, i) => [i + 1, new Set()])));
+class PriorityQueue {
+    constructor() {
+        this.heap = [];
     }
 
-    findMaxST() {
-        let weight = 0;
-
-        // Берём первую попавшуюся вершину.
-        this.#addVertex(this.vertices[0]);
-
-        while (this.notAdded.size && this.edges.size) {
-            const edge = this.#extractMaximum();
-
-            if (this.notAdded.has(edge.end)) {
-                weight += edge.weight;
-
-                this.#addVertex(edge.end);
-            }
-
-            if (this.notAdded.has(edge.start)) {
-                weight += edge.weight;
-
-                this.#addVertex(edge.start);
-            }
-        }
-
-        return this.notAdded.size > 0 ? null : weight;
+    get isEmpty() {
+        return !this.heap.length;
     }
 
-    #extractMaximum() {
-        // Извлекает максимальное ребро из массива рёбер
-        // и больше данного ребра в массива не будет
-        let it = this.edges.values();
-        let max = it.next().value;
+    enqueue(value) {
+        this.heap.push(value);
 
-        for (const edge of this.edges) {
-            if (edge.weight > max.weight) {
-                max = edge;
-            }
-        }
-
-        this.edges.delete(max);
-
-        return max;
+        this.#siftUp(this.heap.length - 1);
     }
 
-    #addVertex(vertex) {
-        this.added.add(vertex);
-        this.notAdded.delete(vertex);
+    dequeue() {
+        const result = this.heap[0];
+        this.heap[0] = this.heap[this.heap.length - 1];
+        this.heap.pop();
 
-        if (!this.adjacencyList.size) {
+        this.#siftDown(0);
+
+        return result;
+    }
+
+    #siftUp(index) {
+        if (index === 0) {
             return;
         }
 
-        for (const edge of this.adjacencyList.get(vertex)) {
-            if (this.notAdded.has(edge.start) || this.notAdded.has(edge.end)) {
-                this.edges.add(edge);
-            }
+        const parentIndex = Math.floor((index - 1) / 2);
+
+        if (this.heap[parentIndex][0] < this.heap[index][0]) {
+            [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
+
+            this.#siftUp(parentIndex);
+        }
+    }
+
+    #siftDown(index) {
+        const left = 2 * index + 1;
+        const right = 2 * index + 2;
+
+        if (left >= this.heap.length) {
+            return;
+        }
+
+        let indexLargest = left;
+        if (right < this.heap.length && compare(this.heap[left], this.heap[right]) > 0) {
+            indexLargest = right;
+        }
+
+        if (this.heap[index][0] < this.heap[indexLargest][0]) {
+            [this.heap[index], this.heap[indexLargest]] = [this.heap[indexLargest], this.heap[index]];
+
+            this.#siftDown(indexLargest);
         }
     }
 }
 
-const vertices = [];
-let verticesNumber, // вершины
-    edgesNumber, // ребра
+spanningTree = (V, E, edges) => {
+    // Create an adjacency list representation of the graph
+    const adj = new Array(V).fill(null).map(() => []);
+
+    // Fill the adjacency list with edges and their weights
+    for (let i = 0; i < E; i++) {
+        const [u, v, wt] = edges[i];
+        adj[u].push([v, wt]);
+        adj[v].push([u, wt]);
+    }
+
+    // Create a priority queue to store edges with their weights
+    const pq = new PriorityQueue();
+
+    // Create a visited array to keep track of visited vertices
+    const visited = new Array(V).fill(false);
+
+    // Variable to store the result (sum of edge weights)
+    let max = 0;
+
+    // Start with vertex 0
+    pq.enqueue([0, 0]);
+
+    // Perform Prim's algorithm to find the Maximum Spanning Tree
+    while (!pq.isEmpty) {
+        const p = pq.dequeue();
+
+        const wt = p[0]; // Weight of the edge
+        const u = p[1]; // Vertex connected to the edge
+
+        if (visited[u]) {
+            continue; // Skip if the vertex is already visited
+        }
+
+        max += wt; // Add the edge weight to the result
+        visited[u] = true; // Mark the vertex as visited
+
+        // Explore the adjacent vertices
+        for (const v of adj[u]) {
+            // v[0] represents the vertex and v[1] represents the edge weight
+            if (!visited[v[0]]) {
+                pq.enqueue([v[1], v[0]]); // Add the adjacent edge to the priority queue
+            }
+        }
+    }
+
+    return visited.some((item) => item === false) ? null : max; // Return the sum of edge weights of the Maximum Spanning Tree
+};
+
+const graph = [];
+let verticesNumber,
+    edgesNumber,
     edgesCounter = 0;
 const readline = require("readline");
 const rl = readline.createInterface({ input: process.stdin });
@@ -95,15 +124,20 @@ rl.on("line", (line) => {
     }
 
     if (edgesCounter < edgesNumber) {
-        const vertex = new Vertex(...line.split(" ").map((item) => +item));
-        vertices.push(vertex);
+        const vertex = line.split(" ").map((item, index) => {
+            if (index < 2) {
+                return +item - 1;
+            }
+
+            return +item;
+        });
+        graph.push(vertex);
 
         edgesCounter++;
     }
 
     if (edgesCounter === edgesNumber) {
-        const graph = new Graph(vertices, verticesNumber);
-        const maxST = graph.findMaxST();
+        const maxST = spanningTree(verticesNumber, edgesNumber, graph);
 
         console.log(maxST === null ? "Oops! I did it again" : maxST);
 
