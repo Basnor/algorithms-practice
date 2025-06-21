c052f8ca652a796d990cff6e571c707149055d21
Basnor <gmbasnor@gmail.com>
2025-06-21 21:26:28 +0500

    add A from basic data structure

--- basicDataStructures/A.js
+++ basicDataStructures/A.js
@@ -1,1 +1,38 @@
-TMP
+const readline = require('readline');
+const rl = readline.createInterface({ input: process.stdin });
+
+let [lineNumber, rowNumber, colNumber] = [0, 0, 0];
+const matrix = [];
+
+rl.on("line", (line) => {
+    if (lineNumber === 0) {
+        rowNumber = +line;
+    }
+    else if (lineNumber === 1) {
+        colNumber = +line;
+    }
+    else if (lineNumber - 2 < rowNumber) {
+        const row = line.split(' ').map((item) => +item);
+
+        for (let i = 0; i <= row.length; i++) {
+            if (matrix[i]) {
+                matrix[i].push(row[i]);
+            }
+            else {
+                matrix[i] = [row[i]];
+            }
+        }
+    }
+
+    lineNumber++;
+
+    if (lineNumber - 2 === rowNumber) {
+        rl.close();
+    }
+});
+
+rl.on("close", () => {
+    for (let i = 0; i < matrix.length; i++) {
+        console.log(matrix[i].join(' '));
+    }
+});
